ea6892eedaa1082c473993c811cfa461fb83fd45
Cloud Lab <mscloudlab@gmail.com>
2022-05-16 11:56:38 -0700

    Updated chemdooble install script to work with newer versions.

--- scripts/setup_chemdoodle.sh
+++ scripts/setup_chemdoodle.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+VERSION=11.9.0
+
 cd molcalc/static/
 
-wget https://web.chemdoodle.com/downloads/ChemDoodleWeb-8.0.0.zip
+wget https://web.chemdoodle.com/downloads/ChemDoodleWeb-${VERSION}.zip
 
-unzip ChemDoodleWeb-8.0.0.zip
+unzip ChemDoodleWeb-${VERSION}.zip
 
 mkdir chemdoodleweb
 
-cp -r ChemDoodleWeb-8.0.0/install/* chemdoodleweb/
-cp -r ChemDoodleWeb-8.0.0/src/* chemdoodleweb/
+cp -r ChemDoodleWeb-${VERSION}/install/* chemdoodleweb/
+cp -r ChemDoodleWeb-${VERSION}/src/* chemdoodleweb/
 
-rm -r ChemDoodleWeb-8.0.0*
+rm -r ChemDoodleWeb-${VERSION}*
 
 
